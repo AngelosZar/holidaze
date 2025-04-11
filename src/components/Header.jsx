535ef3ba9bf5170697485ed52caf019852ee9c96
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-04-11 15:58:58 +0200

    basic layourt for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,33 @@
 import React from 'react';
+import logo1 from '../assets/logos/logo1.svg';
 import { NavLink } from 'react-router-dom';
+
 function Header() {
   return (
     <header className="flex gap-4">
-      <NavLink to="homepage">Homepage</NavLink>
-      <NavLink to="login">Log in</NavLink>
-      <NavLink to="register">Register</NavLink>
-      <NavLink to="profile">Profile</NavLink>
-      <NavLink to="manager">Manager</NavLink>
-      <NavLink to="venue">Venue</NavLink>
+      <div>
+        <NavLink to="homepage">
+          <img src={logo1} alt="logo" className="w-60" />
+        </NavLink>
+      </div>
+
+      <ul className="flex gap-4">
+        <li>
+          <NavLink to="login">Log in</NavLink>
+        </li>
+        <li>
+          <NavLink to="register">Register</NavLink>
+        </li>
+        <li>
+          <NavLink to="profile">Profile</NavLink>
+        </li>
+        <li>
+          <NavLink to="manager">Manager</NavLink>
+        </li>
+        <li>
+          <NavLink to="venue">Venue</NavLink>
+        </li>
+      </ul>
     </header>
   );
 }
