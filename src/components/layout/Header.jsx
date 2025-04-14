2cd6989bcc4671eb1d75f52426387722bec52261
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-04-14 21:03:00 +0200

    header styles and responsiveness

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,79 +1,107 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import logo1 from '../../assets/logos/logo1.svg';
 import { NavLink } from 'react-router-dom';
 
-function Header() {
+export default function Header() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY >= 0) {
+        setIsOpen(false);
+        setIsScrolling(false);
+      }
+      if (window.scrollY > 20) {
+        setIsScrolling(true);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [isOpen, isScrolling]);
+
   return (
-    <header className="flex items-center justify-between bg-white shadow-md max-w-[1520px] px-8 py-2 mx-auto">
-      <div className="flex self-end">
+    <header
+      className={`fixed max-w-[1520px] text-white ${
+        isScrolling ? ' bg-richBlack text-white' : ' bg-richBlack/60 '
+      } mx-auto left-0 right-0`}
+    >
+      <nav className="text-xl font-semi-bold flex items-center justify-between max-w-[1520px] px-8 py-2 mx-auto relative">
         <NavLink to="homepage">
           <img src={logo1} alt="logo" className="w-60" />
         </NavLink>
-      </div>
-      <div className="md:hidden">
-        <HamburgerMenu />
-      </div>
-      <div className="hidden md:block mx-4">
-        <DesktopMenu />
-      </div>
+        <div className="md:hidden">
+          <HamburgerMenu isOpen={isOpen} setIsOpen={setIsOpen} />
+        </div>
+        <div className="hidden md:block">
+          <DesktopMenu />
+        </div>
+      </nav>
     </header>
   );
 }
 
-export default Header;
-
-function HamburgerMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+function HamburgerMenu({ isOpen, setIsOpen }) {
   const toggleMenu = () => {
     setIsOpen(!isOpen);
   };
   return (
     <>
-      <div className="">
-        <button
-          onClick={toggleMenu}
-          className="flex flex-col justify-center items-center gap-2 w-12 h-12 space-y-1.5"
-        >
-          <span
-            className={`block w-6 h-0.5 bg-black transition-transform duration-300 ease-in-out 
-            ${isOpen ? 'rotate-45 translate-y-2' : ''}`}
-          ></span>
-          <span
-            className={`block w-6 h-0.5 bg-black transition-transform duration-300 ease-in-out 
-            ${isOpen ? 'opacity-0 translate-y-2' : ''}`}
-          ></span>
-          <span
-            className={`block w-6 h-0.5 bg-black transition-transform duration-300 ease-in-out 
-            ${isOpen ? '-rotate-45 -translate-y-2' : ''}`}
-          ></span>
-        </button>
-      </div>
-      <div className="z-50">
+      <button
+        onClick={toggleMenu}
+        className={`
+              flex flex-col justify-center items-center gap-1.5 w-12 h-12
+              transition-all duration-300 ease-in-out
+              ${
+                isOpen
+                  ? 'border border-white rounded-full'
+                  : 'border-transparent rounded-none'
+              }
+        
+            `}
+      >
+        <span
+          className={`block h-0.5 bg-white transition-all duration-300 ease-in-out
+            ${
+              isOpen ? 'w-6 -rotate-45 translate-y-1.5' : 'w-4 self-start ml-3'
+            }`}
+        ></span>
+        <span
+          className={`block w-6 h-0.5 bg-white transition-all duration-300 ease-in-out 
+            ${isOpen ? 'opacity-0' : ''}`}
+        ></span>
+        <span
+          className={`block w-6 h-0.5 bg-white transition-all duration-300 ease-in-out 
+            ${isOpen ? 'rotate-45 scale-x-[-1] -translate-y-1.5' : ''}`}
+        ></span>
+      </button>
+
+      <div className="z-50 relative mt-">
         <div
-          className={`fixed left-0 w-full bg-white shadow-lg transition-transform duration-300 ease-in-out ${
+          className={`fixed left-0 w-full top-[88.88px] bg-richBlack/60 transition-transform duration-300 ease-in-out ${
             isOpen ? 'translate-x-0' : '-translate-x-full'
           }`}
-          style={{ top: '5rem' }}
         >
-          <div className="w-full h-full flex flex-col items-center space-y-4 py-4">
-            <ul className=" flex flex-col items-center space-y-4 py-4 text-lg font-medium text-blue-1">
-              <li>
-                <NavLink to="login">Log in</NavLink>
-              </li>
-              <li>
-                <NavLink to="register">Register</NavLink>
-              </li>
-              <li>
-                <NavLink to="profile">Profile</NavLink>
-              </li>
-              <li>
-                <NavLink to="manager">Manager</NavLink>
-              </li>
-              <li>
-                <NavLink to="venue">Venue</NavLink>
-              </li>
-            </ul>
-          </div>
+          <ul className=" flex flex-col items-center space-y-4 py-4 text-lg font-semibold">
+            <li>
+              <NavLink to="login">Log in</NavLink>
+            </li>
+            <li>
+              <NavLink to="register">Register</NavLink>
+            </li>
+            <li>
+              <NavLink to="profile">Profile</NavLink>
+            </li>
+            <li>
+              <NavLink to="manager">Manager</NavLink>
+            </li>
+            <li>
+              <NavLink to="venue">Venue</NavLink>
+            </li>
+          </ul>
         </div>
       </div>
     </>
--- src/components/onHomepage/HomeHero.jsx
+++ src/components/onHomepage/HomeHero.jsx
@@ -1,7 +1,7 @@
 //
 export function HomeHero() {
   return (
-    <section className="relative overflow-hidden">
+    <section className="overflow-hidden">
       {/* <video
               src="https://videos.pexels.com/video-files/1409899/1409899-uhd_2560_1440_25fps.mp4"
               className="w-full h-auto object-cover"
@@ -13,6 +13,7 @@ export function HomeHero() {
       <img
         src="https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         alt="img"
+        className="w-full h-auto object-cover"
       />
     </section>
   );
