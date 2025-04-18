f76befaf777a3a6b6bd296ec55704a72a9bafc5a
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-04-18 17:14:16 +0200

    installed leaflet for map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,11 +1,35 @@
+import React, { useState } from 'react';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { useMap } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Don't forget the CSS
+
 function Map() {
   return (
     <div className="max-w-2xl mx-auto">
-      <img
-        src="https://images.unsplash.com/photo-1524661135-423995f22d0b?q=80&w=1474&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-      />
+      <MyMapComponent />
     </div>
+  );
+}
+
+function MyComponent() {
+  const map = useMap();
+  console.log('map center:', map.getCenter());
+  return null;
+}
+
+function MyMapComponent() {
+  return (
+    <MapContainer
+      center={[59.911491, 10.757933]}
+      zoom={13}
+      style={{ height: '400px', width: '100%' }}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <MyComponent />
+    </MapContainer>
   );
 }
 
