eeb6da9249e756e7cf9fcbb5f27e5f1ebd1cca3e
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-04-22 15:51:49 +0200

    book aside section layout

--- src/components/onSingleVenue/BookAside.jsx
+++ src/components/onSingleVenue/BookAside.jsx
@@ -1,28 +1,60 @@
+import BasicDatePicker from '../onHomepage/DatePicker';
+
 function BookAside() {
   return (
-    <aside>
+    <div className="w-full max-w-lg p-8 bg-white rounded-lg shadow-md border-2 border-primary">
       <h6>Price 1000 nok</h6>
-      <hr />
-      <span>
-        <h6>check in</h6>
-        <h6>check out</h6>
-      </span>
-      <hr />
-      <h6>Number of guests</h6>
-      <p>Dropdown</p>
-      {/* max number of guest for the venue */}
-      <div>
+      <hr className="my-6" />
+      <div className="flex flex-col lg:flex-row gap-4 ">
         <span>
-          <p>Price per night</p> <p>1000nok</p>
-          <p>Price per night</p> <p>1000nok</p>
+          <BasicDatePicker labelMsg={'Check In'} />
         </span>
-        <hr />
         <span>
-          <p>Total</p> <p>1000nok</p>
+          <BasicDatePicker labelMsg={'Check Out'} />
         </span>
       </div>
-    </aside>
+      <hr className="my-6" />
+
+      <NumberDropDown />
+      {/* max number of guest for the venue */}
+      <hr className="my-6" />
+
+      <div className="w-full flex flex-col text-center max-w-sm mx-auto">
+        <button className="btn-primary mb-4">Book</button>
+        <span className="flex flex-row justify-between mt-4">
+          <p>Price per night</p> <p>1000nok</p>
+        </span>
+        <span className="flex flex-row justify-between mt-4">
+          <p>Cleaning fee</p> <p>1000nok</p>
+        </span>
+        <span className="flex flex-row justify-between mt-4">
+          <p>Taxes </p> <p>1000nok</p>
+        </span>
+        <hr className="my-6" />
+        <span className="flex flex-row justify-between mt-4">
+          <p>Total </p> <p>1000nok</p>
+        </span>
+      </div>
+    </div>
   );
 }
 
 export default BookAside;
+
+function NumberDropDown() {
+  return (
+    <div className="flex  gap-8">
+      <label htmlFor="guests" className="text-lg pl-1 pb-1">
+        Number of guests
+      </label>
+      <select>
+        {/* change to number by api- max guests */}
+        {[1, 2, 3, 4, 5, 6].map(number => (
+          <option key={number} value={number}>
+            {number}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
