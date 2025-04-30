523b44e668c8a76ba2f227cf853497cab96709aa
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-04-30 17:08:33 +0200

    custom hook form posting a venue

---
+++ src/hooks/usePostVenue.jsx
@@ -0,0 +1,34 @@
+import { useState, useCallback } from 'react';
+import useVenueStore from '../stores/venuesStore';
+
+function usePostVenue() {
+  const { postVenue } = useVenueStore(state => state);
+  const [isPosting, setIsPosting] = useState(false);
+  const [postError, setPostError] = useState(null);
+
+  const postNewVenue = useCallback(
+    async venueData => {
+      try {
+        setPostError(null);
+        setIsPosting(true);
+        //
+        const data = await postVenue(venueData);
+        console.log('data', data);
+        setIsPosting(false);
+        return data;
+      } catch (error) {
+        setIsPosting(false);
+        setPostError(error);
+        throw error;
+      }
+    },
+    [postVenue]
+  );
+  return {
+    postVenue: postNewVenue,
+    isPosting,
+    postError,
+  };
+}
+
+export default usePostVenue;
