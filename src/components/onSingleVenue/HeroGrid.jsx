86f01eb890d6fbbcfb73eaa0ff2f0ba2ef8e1fe8
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-05-04 18:43:31 +0200

    remake of grid layout on single venue page after fetching data

--- src/components/onSingleVenue/HeroGrid.jsx
+++ src/components/onSingleVenue/HeroGrid.jsx
@@ -1,68 +1,59 @@
-import { useEffect, useState } from 'react';
-
 function HeroGrid({ venueGallery }) {
   return (
     <section className="mt-28 mb-14 relative">
-      <div className="relative overflow-hidden grid grid-cols-1 gap-4 sm:grid-cols-2">
-        <div className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg ">
+      {/* <div className="relative overflow-hidden grid grid-cols-1 gap-4 sm:grid-cols-2"> */}
+      <div
+        className={`relative overflow-hidden h-auto shadow-md rounded-lg ${
+          venueGallery.length >= 1 ? 'grid grid-cols-1 lg:grid-cols-2' : ''
+        }`}
+      >
+        {/* <div className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg "> */}
+        <div className="flex flex-col gap-4 p-4 h-[400px] md:h-[500px] max-h-[800px]">
           <img
             src={venueGallery[0]?.url}
             alt={venueGallery[0]?.alt || 'img'}
-            // src="https://images.unsplash.com/photo-1541274387095-12117e6099dc?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            // alt="img"
-            className="rounded-lg w-full h-auto object-cover"
+            className="rounded-lg w-full h-auto object-cover overflow-hidden "
             style={{ aspectRatio: '1/1' }}
             loading="lazy"
             width="100%"
             height="auto"
           />
-        </div>
-        <div className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg ">
-          <div className="grid-cols-2 gap-4 hidden sm:grid">
-            {venueGallery
-              .slice(1, 5)
-              .map(
-                (image, index) => (
-                  console.log(image),
-                  (
-                    <SingleImgOnGrid
-                      key={index + 1}
-                      img={image.url}
-                      alt={image.alt || 'Venue image'}
-                      index={index}
-                    />
-                  )
-                )
-              )}
-          </div>
-          {/* here seperated */}
-          {/* <div className="grid-cols-2 gap-4 hidden sm:grid"> */}
-          {/* <div className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg max-w-sm">
-              <img
-                src="https://images.unsplash.com/photo-1541274387095-12117e6099dc?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="img"
-                className="rounded-lg w-full h-auto object-cover"
-                style={{ aspectRatio: '1/1' }}
-                loading="lazy"
-                width="100%"
-                height="auto"
-              />
-            </div>
-            <div className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg max-w-sm">
-              <img
-                src="https://images.unsplash.com/photo-1541274387095-12117e6099dc?q=80&w=1365&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                alt="img"
-                className="rounded-lg w-full h-auto object-cover"
-                style={{ aspectRatio: '1/1' }}
-                loading="lazy"
-                width="100%"
-                height="auto"
-              />
-            </div> */}
-        </div>
-        {/* <div className="flex flex-col ">
+          <div
+            className={`flex flex-col absolute bottom-0 left-0 right-0 bg-white p-4 rounded-lg shadow-md ${
+              venueGallery.length >= 1 ? '' : 'hidden'
+            } `}
+          >
             <button className="flex self-end mr-4">See more</button>
-          </div> */}
+            {/* work on see more functionality */}
+          </div>
+        </div>
+        {/* <div className="flex flex-row gap-4 p-4 bg-white shadow-md rounded-lg "> */}
+        <div className=" flex-row gap-4 p-4 bg-white shadow-md rounded-lg justify-around relative hidden sm:flex">
+          {venueGallery.slice(1, 5).map(
+            (image, index) => (
+              console.log(image),
+              (
+                <div className="gap-4 hidden sm:grid">
+                  <SingleImgOnGrid
+                    key={index + 1}
+                    img={image.url}
+                    alt={image.alt || 'Venue image'}
+                    index={index}
+                  />{' '}
+                </div>
+              )
+            )
+          )}
+        </div>
+      </div>
+      {/* venueGallery.length >= 1 */}
+      <div
+        className={`flex flex-col absolute bottom-0 left-0 right-0 bg-white p-4 rounded-lg shadow-md ${
+          venueGallery.length >= 5 ? '' : 'hidden'
+        } `}
+      >
+        <button className="flex self-end mr-4">See more</button>
+        {/* work on see more functionality */}
       </div>
     </section>
   );
@@ -72,10 +63,7 @@ export default HeroGrid;
 
 function SingleImgOnGrid({ img, alt, index }) {
   return (
-    <div
-      className="flex flex-col gap-4 p-4 bg-white shadow-md rounded-lg max-w-sm"
-      key={index}
-    >
+    <div className="flex flex-col gap-4 p-4s rounded-lg max-w-sm" key={index}>
       <img
         src={img}
         alt={alt}
