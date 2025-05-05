d4b281d70f7a5887638d6712697586244de952d5
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-05-05 14:15:56 +0200

    created function to get dates and diff between them

--- src/components/utilities/DateRangeSelector.jsx
+++ src/components/utilities/DateRangeSelector.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import dayjs from 'dayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import BasicDatePickerProvider from '../../providers/BasicDatePickerProvider';
@@ -12,14 +12,75 @@ function DateRangeSelector() {
   const [nights, setNights] = useState(1);
   const [pax, setPax] = useState(1);
 
+  const handleCheckInChange = newValue => {
+    setCheckInDate(newValue);
+  };
+  const handleCheckOutChange = newValue => {
+    setCheckOutDate(newValue);
+  };
+  //
+  useEffect(() => {
+    if (checkInDate) {
+      console.log('checkInDate', checkInDate);
+    }
+    if (checkOutDate) {
+      console.log('checkOutDate', checkOutDate);
+    }
+  }, [checkInDate, checkOutDate]);
+  //
+  useEffect(() => {
+    if (checkInDate && checkOutDate) {
+      const diff = checkOutDate.diff(checkInDate, 'day');
+      setNights(diff);
+      console.log('Nights:', diff);
+    }
+  }, [checkInDate, checkOutDate]);
+
   return (
     <div>
       <BasicDatePickerProvider>
-        <DatePicker label="Check in" />
-        <DatePicker label="Check out" />
+        <DatePicker
+          label="Check in"
+          value={checkInDate}
+          onChange={handleCheckInChange}
+        />
+        <DatePicker
+          label="Check out"
+          value={checkOutDate}
+          onChange={handleCheckOutChange}
+        />
       </BasicDatePickerProvider>
     </div>
   );
 }
 
 export default DateRangeSelector;
+
+// checkInDate
+// Object { "$L": "en", "$u": undefined, "$d": Date Fri May 09 2025 00:00:00 GMT+0200 (Central European Summer Time), "$y": 2025, "$M": 4, "$D": 9, "$W": 5, "$H": 0, "$m": 0, "$s": 0, … }
+// ​
+// "$D": 9
+// ​
+// "$H": 0
+// ​
+// "$L": "en"
+// ​
+// "$M": 4
+// ​
+// "$W": 5
+// ​
+// "$d": Date Fri May 09 2025 00:00:00 GMT+0200 (Central European Summer Time)
+// ​
+// "$isDayjsObject": true
+// ​
+// "$m": 0
+// ​
+// "$ms": 0
+// ​
+// "$s": 0
+// ​
+// "$u": undefined
+// ​
+// "$x": Object {  }
+// ​
+// "$y": 2025
