e29855eb1a27c8ea4b9dd4f23370897847271b1b
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-05-08 15:18:21 +0200

    useEffect to keep state with priceDropDown component

--- src/components/onSingleVenue/BookAside.jsx
+++ src/components/onSingleVenue/BookAside.jsx
@@ -2,13 +2,26 @@ import DateRangeSelector from '../utilities/DateRangeSelector';
 import NumberDropDown from '../utilities/NumberDropDown';
 import datePickerStore from '../../stores/datePickerStore';
 import dayjs from 'dayjs';
+import { use, useEffect, useState } from 'react';
 
 function BookAside({ venue }) {
+  const [alertMessage, setAlertMessage] = useState('');
   const { checkInDate, checkOutDate, nights, pax, reset } = datePickerStore();
 
   let maxNumberOfGuests = venue?.maxGuests;
 
+  useEffect(() => {
+    if (alertMessage && (checkInDate || checkOutDate)) {
+      setAlertMessage('');
+    }
+  }, [checkInDate, checkOutDate, alertMessage]);
+
   function handleButtonClick() {
+    if (!checkInDate || !checkOutDate) {
+      setAlertMessage('No inputs');
+      return;
+    }
+
     const formattedCheckIn =
       checkInDate && typeof checkInDate.format === 'function'
         ? checkInDate.format('YYYY-MM-DD') //check again api format
@@ -29,8 +42,8 @@ function BookAside({ venue }) {
     // submirt booking data
     // reset();
   }
-
-  const isBookButtonDisabled = !checkInDate || !checkOutDate;
+  // or just disable the button???
+  // const isBookButtonDisabled = !checkInDate || !checkOutDate;
 
   return (
     <div className="w-full flex flex-col max-w-md p-8 bg-white rounded-lg shadow-md border-2 border-primary">
@@ -43,12 +56,15 @@ function BookAside({ venue }) {
       <button
         className="btn-primary mb-4 w-full max-w-[60%] mx-auto"
         onClick={handleButtonClick}
-        disabled={isBookButtonDisabled}
+        // disabled={isBookButtonDisabled}
       >
         Book
       </button>
-      {isBookButtonDisabled && (
+      {/* add styles */}
+      {alertMessage === 'No inputs' ? (
         <p>Please select both check-in and check-out dates to book.</p>
+      ) : (
+        ''
       )}
       <PriceDropDown venue={venue} />
     </div>
@@ -58,13 +74,34 @@ function BookAside({ venue }) {
 export default BookAside;
 
 function PriceDropDown({ venue }) {
+  const {
+    nights,
+    pax,
+    pricePerNight,
+    setPricePerNight,
+    totalPrice,
+    calculateTotalPrice,
+  } = datePickerStore();
+  useEffect(() => {
+    if (venue?.price) {
+      setPricePerNight(venue.price);
+    }
+  }, [venue?.price, setPricePerNight]);
+
+  useEffect(() => {
+    calculateTotalPrice();
+  }, [nights, pricePerNight, calculateTotalPrice]);
+
   return (
     <div className="w-full flex flex-col text-center max-w-sm mx-auto">
       <span className="flex flex-row justify-between mt-4">
-        <p>Price per night</p> <p>{venue?.price} nok</p>
+        <p>Price per night</p> <p>{pricePerNight} nok</p>
       </span>
       <span className="flex flex-row justify-between mt-4">
-        <p>Booked days</p> <p>2 days </p>
+        <p>Booked days</p> <p>{nights} </p>
+      </span>
+      <span className="flex flex-row justify-between mt-4 mb-4">
+        <p>Number of guests</p> <p>{pax} </p>
       </span>
       <hr />
       <span className="flex flex-row justify-between mt-4">
@@ -75,7 +112,7 @@ function PriceDropDown({ venue }) {
       </span>
       <hr className="my-6" />
       <span className="flex flex-row justify-between mt-4">
-        <p>Total </p> <p>{venue?.price}</p>
+        <p>Total </p> <p>{totalPrice}</p>
       </span>
     </div>
   );
