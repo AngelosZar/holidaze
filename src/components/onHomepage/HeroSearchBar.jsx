e2c8a5f7c9549641a89b25b861c6a7fc26acd35f
AngelosZar <129210112+AngelosZar@users.noreply.github.com>
2025-05-22 18:19:57 +0200

    started working on search pop overlay

--- src/components/onHomepage/HeroSearchBar.jsx
+++ src/components/onHomepage/HeroSearchBar.jsx
@@ -1,14 +1,16 @@
 // import BasicDatePicker from './DatePicker';
 
-import { useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import DateRangeSelector from '../utilities/DateRangeSelector';
 import useSearchVenues from '../../hooks/useSearchVenues';
 import datePickerStore from '../../stores/datePickerStore';
 import { is } from 'date-fns/locale';
+import StaysList from '../layout/StaysList';
 
 export default function HeroSearchBar() {
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [isSearchPopupOpen, setIsSearchPopupOpen] = useState(false);
   const { stays, loading, error, setPage, setLimit, setSort, searchVenues } =
     useSearchVenues();
   const {
@@ -47,7 +49,7 @@ export default function HeroSearchBar() {
     return !hasConflict;
   };
 
-  const handleSubmitSearchQuery = async function (e) {
+  const handleSubmitSearchQuery = async function () {
     try {
       const data = await searchVenues(searchQuery, 99, 1);
       console.log('search data', data);
@@ -56,20 +58,12 @@ export default function HeroSearchBar() {
         const availableVenues = data.data.filter(venue =>
           checkVenuesAvailability(venue, checkInDate, checkOutDate)
         );
+        setSearchResults(availableVenues);
+        console.log('availableVenues', availableVenues);
 
-        // console.log('availableVenues', availableVenues);
-
-        // data.data.forEach(venue => {
-        //   const isAvailable = checkVenuesAvailability(
-        //     venue,
-        //     checkInDate,
-        //     checkOutDate
-        //   );
-        //   console.log(venue);
-        // });
-
-        return availableVenues;
+        // return availableVenues;
       }
+      console.log(searchResults);
       return [];
     } catch (error) {
       console.error('Error searching venues:', error);
@@ -77,8 +71,9 @@ export default function HeroSearchBar() {
     }
   };
   return (
-    <div
-      className="grid grid-rows-3 grid-cols-1 gap-4 border-2 
+    <>
+      <div
+        className="grid grid-rows-3 grid-cols-1 gap-4 border-2 
                     sm:grid-rows-2 sm:grid-cols-2
                     md:grid-rows-1 md:grid-cols-3 
                     lg:grid-cols-[2fr_2fr_1fr]
@@ -87,27 +82,27 @@ export default function HeroSearchBar() {
                     sm:top-[-100px]
                     md:top-[-200px] 
                     shadow-lg mx-4 sm:mx-auto sm:max-w-[90%]"
-    >
-      <div className="flex flex-col justify-center ">
-        <input
-          type="text"
-          id="searchBar"
-          className="formInput2 relative"
-          placeholder="Search for accommodation"
-          value={searchQuery}
-          onChange={e => setSearchQuery(e.target.value)}
-          onKeyDown={e => {
-            if (e.key === 'Enter') {
-              handleSubmitSearchQuery(e);
-            }
-          }}
-        />
-        {/* <label htmlFor="searchBar" className="text-sm absolute top-0 left-2 ">
+      >
+        <div className="flex flex-col justify-center ">
+          <input
+            type="text"
+            id="searchBar"
+            className="formInput2 relative"
+            placeholder="Search for accommodation"
+            value={searchQuery}
+            onChange={e => setSearchQuery(e.target.value)}
+            onKeyDown={e => {
+              if (e.key === 'Enter') {
+                handleSubmitSearchQuery(e);
+              }
+            }}
+          />
+          {/* <label htmlFor="searchBar" className="text-sm absolute top-0 left-2 ">
           Select a destination
         </label> */}
-      </div>
-      <DateRangeSelector />
-      {/* <div className="flex flex-col lg:flex-row gap-4 ">
+        </div>
+        <DateRangeSelector searchResults={searchResults} />
+        {/* <div className="flex flex-col lg:flex-row gap-4 ">
         <span>
           <BasicDatePicker labelMsg={'Check In'} />
         </span>
@@ -115,14 +110,35 @@ export default function HeroSearchBar() {
           <BasicDatePicker labelMsg={'Check Out'} />
         </span>
       </div> */}
-      <div className="w-full flex justify-center items-center sm:col-span-2 md:col-span-1">
-        <button
-          className="btn btn-primary w-full max-w-[8rem]"
-          onClick={handleSubmitSearchQuery}
-        >
-          Search
-        </button>
+        <div className="w-full flex justify-center items-center sm:col-span-2 md:col-span-1">
+          <button
+            className="btn btn-primary w-full max-w-[8rem]"
+            onClick={handleSubmitSearchQuery}
+          >
+            Search
+          </button>
+        </div>
       </div>
-    </div>
+      {isSearchPopupOpen && <SearchPopUp searchResults={searchResults} />}
+      {/* <SearchPopUp searchResults={searchResults} /> */}
+    </>
+  );
+}
+function SearchPopUp({ searchResults }) {
+  console.log('searchResults', searchResults);
+  useEffect(() => {
+    if (searchResults.length > 0) {
+      console.log(searchResults);
+      // <StaysList stays={stays} loading={loading} error={error} />;
+      // map over the searchResults and display them use card component
+    } else {
+      console.log('No search results found.');
+    }
+  }, [searchResults]);
+
+  return (
+    <section className="fixed inset-0 bg-white z-50">
+      <h1>Search results </h1>
+    </section>
   );
 }
